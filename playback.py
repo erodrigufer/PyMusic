0102386e08e07b226e5bd569b22ce91ac685fb24
Eduardo Rodriguez Fernandez <eduardo.rodriguez@tum.de>
2020-04-04 20:49:16 +0200

    varios sonidos

--- playback.py
+++ playback.py
@@ -1,9 +1,12 @@
 import numpy as np
 import simpleaudio as sa
 
+# Link:
+# https://realpython.com/playing-and-recording-sound-python/
+
 frequency = 440  # Our played note will be 440 Hz
-fs = 44100  # 44100 samples per second
-seconds = 6  # Note duration of 3 seconds
+fs = 44100  # 44100 samples per second (sample rate)
+seconds = 4  # Note duration of 3 seconds
 
 # Generate array with seconds*sample_rate steps, ranging between 0 and seconds
 t = np.linspace(0, seconds, seconds * fs, False)
@@ -11,7 +14,10 @@ t = np.linspace(0, seconds, seconds * fs, False)
 # Generate a 440 Hz sine wave
 note = np.sin(frequency * t * 2 * np.pi) + np.sin(2.7*frequency * t * 2 * np.pi)
 
+note3 = np.sin(1.7*frequency * t * 2 * np.pi)
+
 envelope = np.exp(-t)
+#envelope = 1
 
 note2 = np.multiply(note,envelope) # element-wise multiplication
 
@@ -20,9 +26,16 @@ audio = note2 * (2**15 - 1) / np.max(np.abs(note2))
 # Convert to 16-bit data
 audio = audio.astype(np.int16)
 
+audio2 = note3 * (2**15 - 1) / np.max(np.abs(note3))
+audio2 = audio2.astype(np.int16)
+
 
 # Start playback
 play_obj = sa.play_buffer(audio, 1, 2, fs)
 
 # Wait for playback to finish before exiting
 play_obj.wait_done()
+
+play_obj = sa.play_buffer(audio2, 1, 2, fs)
+play_obj.wait_done()
+
