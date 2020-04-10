7de7bb533bbcd221061fc7b67ac861f8a60f9c19
Eduardo Rodriguez Fernandez <eduardo.rodriguez@tum.de>
2020-04-10 13:51:07 +0200

    nuevo method para crear envelope y agregar frecuencias de la campanilla

--- sonidillo.py
+++ sonidillo.py
@@ -7,7 +7,9 @@ import simpleaudio as sa
 
 # GLOBAL VARIABLES
 
-frequencies = [210,265,234,171]
+bass_cac_de_moun = [210,265,234,171] # sonido como de bass caca de mono
+campanilla = [437.5,875,1180,1422 ] # campanilla fire
+frequencies = campanilla
 fs = 44100  # 44100 samples per second (sample rate)
 seconds = 2  # Note duration of 3 seconds
 pi = np.pi
@@ -16,15 +18,21 @@ t = np.linspace(0, seconds, seconds * fs, False) # Generate array with seconds*s
 
 
 # METHODS
+
+# Creates sine wave with specific frequency
 def generate_wave(frequency):
     return np.sin(frequency * t * 2 * pi)
 
-# Adds a sine wave from each frequency
+# Adds a sine wave from each frequency in frequencies
 def create_note(frequencies):
-        return sum([generate_wave(frequency) for frequency in frequencies]) 
+    return sum([generate_wave(frequency) for frequency in frequencies]) 
 
-def apply_envelope(note,envelope):
-    return np.multiply(note,envelope)
+# Creates envelope with exponential function to apply to note
+def create_envelope():
+    return np.exp(-t)
+
+def apply_envelope(note):
+    return np.multiply(note,create_envelope()) # element-wise multiplication
 
 def get_first_step(note, step):
     if step:
@@ -62,9 +70,9 @@ def play_audio(audio):
 def start_synth():
     note = create_note(frequencies)
 
-    envelope = np.exp(-t)
+    #envelope = np.exp(-t)
 
-    note = apply_envelope(note,envelope)
+    note = apply_envelope(note)
 
     loop = create_loop(note,steps)
 
