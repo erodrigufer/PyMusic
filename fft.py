c5f5ded0fb1c8016e915c7ac56c69ac0d0e0c356
Eduardo Rodriguez Fernandez <eduardo.rodriguez@tum.de>
2020-04-12 23:20:16 +0200

    fft update

--- fft.py
+++ fft.py
@@ -11,7 +11,8 @@ T = 1.0 / 800.0 # sample period
 t = np.linspace(0.0, N*T, N)
 y = np.sin(50.0 * 2.0*np.pi*t) + 0.5*np.sin(80.0 * 2.0*np.pi*t)
 yf = scipy.fft(y)
-xf = np.linspace(0.0, 1.0/(2.0*T), N//2)
+xf = np.linspace(0.0, 1.0/(2.0*T), N//2) 
+# // is the operator for a floor division
 import matplotlib.pyplot as plt
 plt.plot(xf, 2.0/N * np.abs(yf[0:N//2]))
 plt.grid()
